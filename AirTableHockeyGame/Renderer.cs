72d530c46450a97ece9d3dafd1eeb3d0882c5a19
Rodyna Amr <130939400+rodynaamrfathy@users.noreply.github.com>
2024-09-25 13:08:32 +0300

    Update Renderer.cs

--- AirTableHockeyGame/Renderer.cs
+++ AirTableHockeyGame/Renderer.cs
@@ -1,16 +1,8 @@
-﻿using AirTableHockeyGame;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Shapes;
 
 namespace AirTableHockeyGame
 {
-    internal class Renderer
+    public class Renderer
     {
         private Canvas canvas;
 
@@ -21,7 +13,15 @@ namespace AirTableHockeyGame
 
         public void AddShapeToCanvas(Ball shape)
         {
-            canvas.Children.Add(shape.DrawingShape);
+            if (!canvas.Children.Contains(shape.DrawingShape))
+            {
+                canvas.Children.Add(shape.DrawingShape);
+            }
+        }
+
+        public void UpdateCanvas()
+        {
+            // This can be extended later for more advanced rendering
         }
 
         public void UpdateCanvas(Ball shape)
@@ -29,29 +29,5 @@ namespace AirTableHockeyGame
             Canvas.SetLeft(shape.DrawingShape, shape.Position.X);
             Canvas.SetTop(shape.DrawingShape, shape.Position.Y);
         }
-
-        public void SetInitialShapePosition(Ball shape)
-        {
-            Canvas.SetLeft(shape.DrawingShape, (float)0.5 * (canvas.ActualWidth + shape.Radius));
-            Canvas.SetTop(shape.DrawingShape, (float)canvas.ActualHeight - shape.Radius);
-        }
-
-        public void UpdateCanvas()
-        {
-            foreach (UIElement child in canvas.Children)
-            {
-                if (child is Shape drawingShape)
-                {
-                    var shape = canvas.Children.OfType<Ball>().FirstOrDefault(s => s.DrawingShape == drawingShape);
-                    if (shape != null)
-                    {
-                        UpdateCanvas(shape);
-                    }
-                }
-            }
-        }
     }
-
-
-
 }
